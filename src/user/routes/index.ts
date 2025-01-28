825a2b46e26fdeb553225c1ed519df0de70c6908
willian <williandeitosi@gmail.com>
2025-01-28 17:51:07 -0300

    feat: add route to register user

---
+++ src/user/routes/index.ts
@@ -0,0 +1,14 @@
+import type { Application } from "express";
+import { UserController } from "../controllers";
+import { UserService } from "../services";
+
+const userService = new UserService();
+const userController = new UserController(userService);
+
+const userRoutes = (app: Application) => {
+  app.post("/register", (req, res) => {
+    userController.register(req, res);
+  });
+};
+
+export default userRoutes;
