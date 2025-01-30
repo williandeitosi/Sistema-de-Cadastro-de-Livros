5ab72aa5f7b1d4c3b09fecb4a0add8387d7660fc
willian <williandeitosi@gmail.com>
2025-01-30 12:12:33 -0300

    feat: add login user service

--- src/user/services/index.ts
+++ src/user/services/index.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { db } from "../../../prisma/db";
-import type { UsersTypes } from "../schema/user-schema";
+import { env } from "../../config/dot-env-config";
+import type { LoginTypes, UsersTypes } from "../schema/user-schema";
 
 export class UserService {
   private async findByEmail(email: string) {
@@ -22,5 +24,29 @@ export class UserService {
     const { password: _, ...userWithoutPassword } = newUser;
 
     return userWithoutPassword;
+  }
+
+  async loginUser({ email, password }: LoginTypes) {
+    const user = await this.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Usuário não existe");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new Error("Credenciais inválidas");
+    }
+
+    const payload = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    const token = jwt.sign(payload, env.JWT_SECRET, { expiresIn: "1h" });
+
+    return { user: payload, token };
   }
 }
