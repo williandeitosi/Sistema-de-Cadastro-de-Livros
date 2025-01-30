2d8c3dbd1ed9762c21efcad08ca1743e798e966a
willian <williandeitosi@gmail.com>
2025-01-30 16:48:43 -0300

    feat: add book routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import type { Application } from "express";
+import bookRoutes from "./book/routes";
 import userRoutes from "./user/routes";
 
 const setupRoutes = (app: Application) => {
   userRoutes(app);
+  bookRoutes(app);
 };
 
 export default setupRoutes;
